06017763974a46e7a5992b21fe8358f6ad1874cc
Abhinay <abhinayy060@gmail.com>
2023-07-28 01:15:34 +0530

    Fix line endings and remove embedded repository

---
+++ backend/base/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+from .models import Product
+from .products import products
+from .serializers import ProductSerializer
+
+# Create your views here.
+@api_view(['GET'])
+
+def getRoutes(request):
+    routes = [
+        '/api/products/',
+        '/api/products/create/',
+        '/api/products/upload/',
+        '/api/products/<id>/reviews/',
+        '/api/products/top/',
+        '/api/products/<id>/',
+        '/api/products/delete/<id>/',
+        '/api/products/<update>/<id>/',
+        
+        
+        
+    ]
+    return Response(routes)
+@api_view(['GET'])
+def getProducts(request):
+    products = Product.objects.all() #it returns all products objects from models.After completion of model setup we can apply this while serializing the data.
+    serializer = ProductSerializer(products, many=True) #-We get error here like serializer so apply serialization.
+    return Response(serializer.data)
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    
+    
+    return Response(serializer.data)
+    
